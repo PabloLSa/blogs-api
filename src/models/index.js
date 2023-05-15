c80b35dd2f28bfb9e31a0a6a969dd0e2a6204d1b
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-05-15 14:19:59 -0300

    Start
     Start Project Blogs API

--- src/models/index.js
+++ src/models/index.js
@@ -7,7 +7,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
-
+// start of code to connect to heroku
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
