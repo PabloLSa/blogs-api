b784ab1d9b886638e71b3a57be6b8b093a063fd4
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-05-22 20:58:54 -0300

    User
    [200~Crie o modelo User em src/models/User.js com as propriedades corretas

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id:{
+      type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false
+    },
+  displayName:{
+    type: DataTypes.STRING, allowNull: false 
+  },
+  email:{
+    type: DataTypes.STRING, allowNull: false 
+  },
+  password:{
+    type: DataTypes.STRING, allowNull: false 
+  },
+  image:{
+    type: DataTypes.STRING, allowNull: false 
+  },
+  }, {
+    tableName: 'user', underscored: true, timestamps: false,
+  });
+
+  // User.associate = (models) => {
+  //   User.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'posts' } )
+  // }
+  return User;
+
+}
+
