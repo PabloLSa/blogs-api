37eb1cd1611678867c3763f68486799d782a5a3e
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-05-25 14:43:01 -0300

    Model Category
    Criei o modelo Category em src/models/Category.js

---
+++ src/models/Category.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+  }, {
+    timestamps: false,
+    tableName: 'categories',
+  });
+
+  return Category;
+};
