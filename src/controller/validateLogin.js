8f6f46e3622ec49f02e57f836e6e18806e7461fd
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-05-25 09:29:53 -0300

    endpoint
     correção de codigo (const { createToken } = require(../auth/generateToken); )  endpoint POST /login

--- src/controller/validateLogin.js
+++ src/controller/validateLogin.js
@@ -1,5 +1,5 @@
 const { user } = require('../services/user.service');
-const createToken = require('../auth/generateToken');
+const { createToken } = require('../auth/generateToken');
 
 const login = async (req, res) => {
   const { email, password } = req.body;
